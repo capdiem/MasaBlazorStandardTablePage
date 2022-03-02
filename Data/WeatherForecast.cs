103218785781e503fe30ee62cdcb38c8ecc9fc46
capdiem <capdiem@live.com>
2022-03-03 00:36:53 +0800

    🆕 feat: table

--- Data/WeatherForecast.cs
+++ Data/WeatherForecast.cs
@@ -9,5 +9,19 @@
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+
+        public WarningSigns? WarningSigns
+        {
+            get
+            {
+                return TemperatureC switch
+                {
+                    >= 39 => Data.WarningSigns.Red,
+                    >= 37 => Data.WarningSigns.Orange,
+                    >= 35 => Data.WarningSigns.Yellow,
+                    _ => null
+                };
+            }
+        }
     }
 }
